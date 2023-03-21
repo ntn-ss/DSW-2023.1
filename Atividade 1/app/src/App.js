c40e9f83683b3d79839db44e86ad6f55c156207c
ntn-ss <nathandossantosdasilva@gmail.com>
2023-03-21 18:25:29 -0300

    Segunda Aula

--- Atividade 1/app/src/App.js
+++ Atividade 1/app/src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
-import MeusDados from './components/atividade00/01MeusDados';
-import MeusDados2 from './components/atividade00/02MeusDados';
-import Temperatura from './components/atividade00/03Temperatura';
+import MeusDados from './atividades/atividade00/01MeusDados';
+import MeusDados2 from './atividades/atividade00/02MeusDados';
+import Temperatura from './atividades/atividade00/03Temperatura';
+import FunctionB from './components/04Hierarquia/functionB';
+// import { Header as H, Body as B, Footer as F } from './components/05Multiplo';
+import * as Site from './components/05Multiplo';
+import {Supermercado, Legume, Fruta, Bebida} from './components/06Children';
 
 function App() {
   return (
@@ -9,6 +13,21 @@ function App() {
       <MeusDados />
       <MeusDados2 nome="Nathan" curso="Design Digital" universidade="UFC"/>
       <Temperatura />
+      <FunctionB sobrenome="Pagodinho"/>
+      {/* <H />
+      <B />
+      <F /> */}
+
+      <Site.Header />
+      <Site.Body />
+      <Site.Footer />
+
+      <Supermercado nome="Mercado">
+        <Legume nome = "Batata" />
+        <Fruta nome = "Maçã" />
+        <Bebida nome = "Café" />
+        <Bebida nome = "Cajuína São Geraldo" />
+      </Supermercado>
     </div>
   );
 }
